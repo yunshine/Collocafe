a96ec1ccbc1f9e002f92698c55a707815ae810fd
Yun Chung <yun.chung@mac.com>
2020-10-03 13:33:21 +0900

    Created and rendered the cafes index page

--- app.js
+++ app.js
@@ -7,13 +7,22 @@ app.use(bodyParser.urlencoded({ extended: true }))
 // app.set('view engine', 'ejs');
 
 //  ***** ROUTES *****
+
+// Root Route
 app.get('/', function (req, res) {
   res.send('This is the root route...');
 });
 
+// Index Route
+app.get('/cafes', function (req, res) {
+  res.render('index.ejs');
+});
+
+// Default Route
 app.get('*', function (req, res) {
   res.send('Are you lost?');
 });
+
 
 app.listen(3000, function () {
   console.log('The server has started...');
