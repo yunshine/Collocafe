60f50682fb9444e5053cda63915efc4936fe729a
Yun Chung <yun.chung@mac.com>
2020-10-03 14:47:17 +0900

    Created functional new cafe form

--- app.js
+++ app.js
@@ -29,6 +29,31 @@ app.get('/cafes', function (req, res) {
   res.render('index.ejs', { cafes: cafes });
 });
 
+// New Route - goes to new cafe form
+app.get("/cafes/new", function (req, res) {
+  res.render("new.ejs");
+});
+
+// Create Route - makes and saves a new cafe
+app.post("/cafes", function (req, res) {
+  // get data from form and add to cafes array
+  var name = req.body.name;
+  var area = req.body.area;
+  var newCafe = { name: name, area: area }
+  cafes.push(newCafe);
+  res.redirect("/cafes");
+
+  // *****  Create a new campground and save to DB  *****
+  // Cafe.create(newCafe, function (err, newlyCreated) {
+  //   if (err) {
+  //     console.log(err);
+  //   } else {
+  //     //redirect back to cafes page
+  //     res.redirect("/cafes");
+  //   }
+  // });
+});
+
 // Default Route
 app.get('*', function (req, res) {
   res.send('Are you lost?');
