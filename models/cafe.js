42fa9d16ae7a8279b856363e285c716ed88531d8
Yun Chung <yun.chung@mac.com>
2020-11-11 19:40:45 +0900

    Added image carousel to show page

--- models/cafe.js
+++ models/cafe.js
@@ -2,32 +2,32 @@
 const mongoose = require('mongoose');
 
 const cafeSchema = new mongoose.Schema({
-  name: String,
-  area: String,
-  images: [
-    {
-      url: String,
-      filename: String
-    }
-  ],
-  // how to set a default value in the schema...
-  // created:  {type: Date, default: Date.now},
-  // One to Many database association (this is an example of object reference association vs. embedded data association) with comments...
+    name: String,
+    area: String,
+    images: [
+        {
+            url: String,
+            filename: String
+        }
+    ],
+    // how to set a default value in the schema...
+    // created:  {type: Date, default: Date.now},
+    // One to Many database association (this is an example of object reference association vs. embedded data association) with comments...
 
-  author: {
-    id: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User"
+    author: {
+        id: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User"
+        },
+        username: String
     },
-    username: String
-  },
 
-  comments: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Comment", // refers to the model that we're referring to...
-    }
-  ],
+    comments: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Comment", // refers to the model that we're referring to...
+        }
+    ],
 
 });
 
--- routes/cafes.js
+++ routes/cafes.js
@@ -16,18 +16,18 @@ const upload = multer({ storage });
 // =======================================================================
 // INDEX Route
 router.get('/cafes', async (req, res) => {
-  // *** Get all cafes from DB ***
-  Cafe.find(function (err, allCafes) {
-    // const allCafes = await Cafe.find({});
-    if (err) {
-      console.log(err);
-      req.flash('error', "Cafes could not be found...");
-      res.redirect("/cafes");
-    } else {
-      // the req.user below is needed to check if the user is logged in or not...
-      res.render('cafes/index.ejs', { cafes: allCafes });
-    }
-  });
+    // *** Get all cafes from DB ***
+    Cafe.find(function (err, allCafes) {
+        // const allCafes = await Cafe.find({});
+        if (err) {
+            console.log(err);
+            req.flash('error', "Cafes could not be found...");
+            res.redirect("/cafes");
+        } else {
+            // the req.user below is needed to check if the user is logged in or not...
+            res.render('cafes/index.ejs', { cafes: allCafes });
+        }
+    });
 });
 // router.get('/cafes', function (req, res) {
 //   res.render('index.ejs', { cafes: cafes });
@@ -35,41 +35,41 @@ router.get('/cafes', async (req, res) => {
 
 // NEW Route - goes to new cafe form
 router.get("/cafes/new", middleware.isLoggedIn, (req, res) => {
-  res.render("cafes/new.ejs");
+    res.render("cafes/new.ejs");
 });
 
 // CREATE Route - makes and saves a new cafe to the DB
 router.post("/cafes", middleware.isLoggedIn, upload.array('image'), (req, res) => {
-  // *** gets SANITIZED data from new cafe form and adds to Cafe DB ***
-  let name = req.sanitize(req.body.name);
-  let area = req.sanitize(req.body.area);
-  // let image = req.sanitize(req.body.image);
-  let newCafe = {
-    name: name,
-    area: area,
-    images: req.files.map(f => ({ url: f.path, filename: f.filename })),
-    author: {
-      id: req.user._id,
-      username: req.user.username,
-    }
-  };
-  // req.body.cafe.body = req.sanitize(req.body.cafe.body);
-  // *** Makes and saves a new cafe to the Cafe DB ***
-  Cafe.create(newCafe, (err, cafe) => {
-    if (err) {
-      console.log(err);
-      req.flash('error', "There was an error, and that cafe could not be created...");
-      res.redirect("/cafes");
-      // or...   res.render("new.ejs");
-    } else {
-      console.log("New Cafe: ", cafe);
-      req.flash('success', "Your cafe has been added.");
-      console.log(newCafe)
-      res.redirect("/cafes");
-    }
-  });
-  // cafes.push(newCafe);
-  // res.redirect("/cafes");
+    // *** gets SANITIZED data from new cafe form and adds to Cafe DB ***
+    let name = req.sanitize(req.body.name);
+    let area = req.sanitize(req.body.area);
+    // let image = req.sanitize(req.body.image);
+    let newCafe = {
+        name: name,
+        area: area,
+        images: req.files.map(f => ({ url: f.path, filename: f.filename })),
+        author: {
+            id: req.user._id,
+            username: req.user.username,
+        }
+    };
+    // req.body.cafe.body = req.sanitize(req.body.cafe.body);
+    // *** Makes and saves a new cafe to the Cafe DB ***
+    Cafe.create(newCafe, (err, cafe) => {
+        if (err) {
+            console.log(err);
+            req.flash('error', "There was an error, and that cafe could not be created...");
+            res.redirect("/cafes");
+            // or...   res.render("new.ejs");
+        } else {
+            console.log("New Cafe: ", cafe);
+            req.flash('success', "Your cafe has been added.");
+            console.log(newCafe)
+            res.redirect("/cafes");
+        }
+    });
+    // cafes.push(newCafe);
+    // res.redirect("/cafes");
 });
 
 // CREATE Route using CLOUDINARY...
@@ -77,20 +77,20 @@ router.post("/cafes", middleware.isLoggedIn, upload.array('image'), (req, res) =
 
 // SHOW Route - shows one cafe
 router.get("/cafes/:id", (req, res) => {
-  // *** Finds a cafe in the DB by its id & passes that cafe to the show page***
-  // Cafe.findById(req.params.id, function (err, foundCafe) {
+    // *** Finds a cafe in the DB by its id & passes that cafe to the show page***
+    // Cafe.findById(req.params.id, function (err, foundCafe) {
 
-  // *** Finds a cafe in the DB by its id & passes that cafe to the edit page with associated comments using .populate("comments").exec ***
-  Cafe.findById(req.params.id).populate("comments").exec(function (err, foundCafe) {
-    if (err) {
-      console.log(err);
-      req.flash('error', "There was an error, and that cafe could not be found...");
-      res.redirect("/cafes");
-    } else {
-      // renders the show page view with the one cafe from the DB
-      res.render("cafes/show.ejs", { cafe: foundCafe });
-    }
-  });
+    // *** Finds a cafe in the DB by its id & passes that cafe to the edit page with associated comments using .populate("comments").exec ***
+    Cafe.findById(req.params.id).populate("comments").exec(function (err, foundCafe) {
+        if (err) {
+            console.log(err);
+            req.flash('error', "There was an error, and that cafe could not be found...");
+            res.redirect("/cafes");
+        } else {
+            // renders the show page view with the one cafe from the DB
+            res.render("cafes/show.ejs", { cafe: foundCafe });
+        }
+    });
 });
 
 // EDIT Route - goes to edit cafe form
@@ -120,58 +120,58 @@ router.get("/cafes/:id", (req, res) => {
 
 // EDIT Route - goes to edit cafe form
 router.get("/cafes/:id/edit", middleware.checkCafeOwnership, (req, res) => {
-  // Q: first, before editing, is the user logged in? Use middleware, then...
-  // *** ...find a cafe in the DB by its id & pass that cafe to the edit page***
-  Cafe.findById(req.params.id, function (err, foundCafe) {
-    if (err) {
-      console.log(error);
-      req.flash('error', "There was an error, and that cafe could not be found...");
-      res.redirect("/cafes");
-    } else {
-      // middleware has already checked if the user is the cafe's author...
-      res.render('cafes/edit.ejs', { cafe: foundCafe });
-    }
-  });
+    // Q: first, before editing, is the user logged in? Use middleware, then...
+    // *** ...find a cafe in the DB by its id & pass that cafe to the edit page***
+    Cafe.findById(req.params.id, function (err, foundCafe) {
+        if (err) {
+            console.log(error);
+            req.flash('error', "There was an error, and that cafe could not be found...");
+            res.redirect("/cafes");
+        } else {
+            // middleware has already checked if the user is the cafe's author...
+            res.render('cafes/edit.ejs', { cafe: foundCafe });
+        }
+    });
 });
 
 // UPDATE Route - saves the updated info about one cafe into the DB
 // Q: first, before editing, is the user logged in? Use middleware...
 router.put("/cafes/:id", middleware.checkCafeOwnership, upload.array('image'), (req, res) => {
-  // *** gets SANITIZED data from edit cafe form and updates the Cafe DB ***
-  let name = req.sanitize(req.body.name);
-  let area = req.sanitize(req.body.area);
-  // let image = req.sanitize(req.body.image);
-  let images = req.files.map(f => ({ url: f.path, filename: f.filename }));
-  let newCafe = { name: name, area: area, images: images }
+    // *** gets SANITIZED data from edit cafe form and updates the Cafe DB ***
+    let name = req.sanitize(req.body.name);
+    let area = req.sanitize(req.body.area);
+    // let image = req.sanitize(req.body.image);
+    let images = req.files.map(f => ({ url: f.path, filename: f.filename }));
+    let newCafe = { name: name, area: area, images: images }
 
-  // the logic to update the info...
-  Cafe.findByIdAndUpdate(req.params.id, newCafe, function (err, updatedCafe) {
-    if (err) {
-      console.log(error);
-      req.flash('error', "That cafe could not be found.");
-      res.redirect(`/cafes/${req.params.id}`);
-    } else {
-      req.flash('success', "This cafe has been updated.");
-      res.redirect(`/cafes/${req.params.id}`);
-    }
-  });
+    // the logic to update the info...
+    Cafe.findByIdAndUpdate(req.params.id, newCafe, function (err, updatedCafe) {
+        if (err) {
+            console.log(error);
+            req.flash('error', "That cafe could not be found.");
+            res.redirect(`/cafes/${req.params.id}`);
+        } else {
+            req.flash('success', "This cafe has been updated.");
+            res.redirect(`/cafes/${req.params.id}`);
+        }
+    });
 });
 
 // DELETE Route
 // Q: first, before editing, is the user logged in? Use middleware...
 router.delete("/cafes/:id", middleware.checkCafeOwnership, (req, res) => {
-  //destroy blog
-  Cafe.findByIdAndRemove(req.params.id, function (err) {
-    if (err) {
-      console.log(error);
-      req.flash('error', "That cafe could not be found.");
-      res.redirect(`/cafes/${req.params.id}`);
-    } else {
-      req.flash('success', "Your cafe has been deleted.");
-      res.redirect("/cafes");
-    }
-  })
-  //redirect somewhere
+    //destroy blog
+    Cafe.findByIdAndRemove(req.params.id, function (err) {
+        if (err) {
+            console.log(error);
+            req.flash('error', "That cafe could not be found.");
+            res.redirect(`/cafes/${req.params.id}`);
+        } else {
+            req.flash('success', "Your cafe has been deleted.");
+            res.redirect("/cafes");
+        }
+    })
+    //redirect somewhere
 });
 
 // // Lots of actions and routes need to check if a user is looged in or not. So, use middleware (like this below...) & use it wherever needed (ie. creating comments)...
